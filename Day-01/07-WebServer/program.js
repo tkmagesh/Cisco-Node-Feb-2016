e46a7dbbb6ac9a36a934b4a5e4c2647aa68f2b39
tkmagesh <tkmagesh77@gmail.com>
2016-02-16 16:09:00 +0530

    Day-01 Complete

--- Day-01/07-WebServer/program.js
+++ Day-01/07-WebServer/program.js
@@ -1,8 +1,19 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path');
+
 var server = http.createServer(function(req, res){
-    console.log(req.url);
-    res.write('welcome to node.js');
-    res.end();
+    req.url = req.url === '/' ? '/index.html' : req.url;
+    var resourcePath = path.join(__dirname, req.url);
+    fs.access(resourcePath, fs.F_OK | fs.R_OK, function(err) {
+        if (err){
+            res.statusCode = 404;
+            res.end();
+            return;
+        }
+        var stream = fs.createReadStream(resourcePath);
+        stream.pipe(res);
+    });
 });
 server.listen(8080);
 console.log('server listening on port 8080!');
