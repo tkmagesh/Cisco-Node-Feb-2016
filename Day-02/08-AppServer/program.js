5957b329783b9b0246dedcc0b9139640c1e2a0cf
tkmagesh <tkmagesh77@gmail.com>
2016-02-17 10:31:56 +0530

    Refactoring - assignment

--- Day-02/08-AppServer/program.js
+++ Day-02/08-AppServer/program.js
@@ -1,10 +1,18 @@
 var http = require('http'),
     fs = require('fs'),
     url = require('url'),
+    qs = require('querystring'),
     calculator = require('./calculator'),
     path = require('path');
 
 //http://localhost:8080/calculator?op=add&n1=100&n2=200
+
+/*
+- dataParser
+- staticServer
+- calculatorHandler
+- notFoundHandler
+*/
 
 var staticExtns = ['.html', '.css', '.js', '.xml', '.png', '.ico', '.json', '.txt'];
 function isStatic(pathname){
@@ -26,14 +34,30 @@ var server = http.createServer(function(req, res){
             var stream = fs.createReadStream(resourcePath);
             stream.pipe(res);
         });
-    } else if (urlObj.pathname === '/calculator'){
+    } else if (urlObj.pathname === '/calculator' && req.method === 'GET'){
         var query = urlObj.query,
-            op = query.op,
-            n1 = parseInt(query.n1, 10),
-            n2 = parseInt(query.n2, 10);
+            op = query.op || 'add',
+            n1 = parseInt(query.n1, 10) || 0,
+            n2 = parseInt(query.n2, 10) || 0;
         var result = calculator[op](n1, n2);
+
         res.write(result.toString());
         res.end();
+    } else if (urlObj.pathname === '/calculator' && req.method === 'POST'){
+        var reqData = '';
+        req.on('data', function(chunk){
+            reqData += chunk;
+        });
+        req.on('end', function(){
+            var query = qs.parse(reqData),
+                op = query.op || 'add',
+                n1 = parseInt(query.n1, 10) || 0,
+                n2 = parseInt(query.n2, 10) || 0;
+            var result = calculator[op](n1, n2);
+
+            res.write(result.toString());
+            res.end();
+        });
     } else {
         res.statusCode = 404;
         res.end();
