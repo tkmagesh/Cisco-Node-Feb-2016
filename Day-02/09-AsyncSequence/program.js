fb6be6a33ac33a0e6b9502f9514fdaf7acbc3fe1
tkmagesh <tkmagesh77@gmail.com>
2016-02-17 11:58:39 +0530

    async sequence - solution

--- Day-02/09-AsyncSequence/program.js
+++ Day-02/09-AsyncSequence/program.js
@@ -18,47 +18,58 @@ function f4Sync(){
     console.log('f4Sync completed');
 }
 
+var syncFns = [f1Sync, f2Sync, f3Sync, f4Sync];
+
 function runSync(){
-    f1Sync();
-    f2Sync();
-    f3Sync();
-    f4Sync();
+    for(var i=0; i<syncFns.length; i++)
+        syncFns[i]();
 }
 module.exports.runSync = runSync;
 
-function f1(){
+function f1(next){
     console.log('f1 triggered');
     setTimeout(function(){
         console.log('f1 completed');
+        if (next) next();
     }, 3000);
 }
 
-function f2(){
+function f2(next){
     console.log('f2 triggered');
     setTimeout(function(){
         console.log('f2 completed');
+        if (next) next();
     }, 3000);
 }
 
-function f3(){
+function f3(next){
     console.log('f3 triggered');
     setTimeout(function(){
         console.log('f3 completed');
+        if (next) next();
     }, 3000);
 }
 
-function f4(){
+function f4(next){
     console.log('f4 triggered');
     setTimeout(function(){
         console.log('f4 completed');
+        if (next) next();
     }, 3000);
 }
 
+var fns = [f1, f3, f2, f4];
+
 function run(){
-    f1();
-    f2();
-    f3();
-    f4();
+    function exec(fns){
+        var first = fns[0],
+            remaining = fns.slice(1),
+            next = function(){
+                exec(remaining);
+            };
+        if (first) first(next);
+    }
+    exec(fns);
 }
 
 module.exports.run = run;
